e5a45d468890b0b14a3e7016c45d3d7c46cddb1b
AnnaKoreneva <anna.koreneva1409@gmail.com>
2024-10-30 19:24:41 -0700

    added the tests for mocking response

---
+++ tests/deleteArticle.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect, request } from "@playwright/test";
+import tags from "../data/tags.json";
+import { mockArticle } from '../data/article'
+
+test.beforeEach("go to the web-portal", async ({ page }) => {
+    await page.goto("https://conduit.bondaracademy.com/");
+    await page.getByText("Sign In").click();
+    await page.getByPlaceholder("Email").fill("pwtest1409@test.com");
+    await page.getByPlaceholder("Password").fill("pwtest1409");
+    await page.getByRole('button', { name: "Sign in" }).click();
+});
+
+test("delete article", async ({ page, request }) => {
+    const response =  await request.post(
+      "https://conduit-api.bondaracademy.com/api/users/login",
+      {
+        data: {user: { email: "pwtest1409@test.com", password: "pwtest1409" }},
+        });  
+    const responseBody = await response.json()
+    const accessToken = responseBody.user.token;
+
+    const responseArticleCreation = await request.post("https://conduit-api.bondaracademy.com/api/articles/", {
+      data: {
+        article: {
+          title: "QA Article Demo Title",
+          description: "QA Article Demo Topic",
+          body: "QA Article Demo Text",
+          tagList: ["tag1", "tag2", "tag3"],
+        },
+      },
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    });
+    expect(responseArticleCreation.status()).toEqual(201)
+    await page.getByText("Global Feed").click();
+    await page.getByText("QA Article Demo Title").click();
+    await page
+      .locator(".banner")
+      .getByRole("button", { name: "Delete Article" })
+      .click();
+    expect(await page.locator("app-article-preview h1").first().textContent()).not.toContain("QA Article Demo Title");
+});
---
+++ tests/mockResponse.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+import tags from "../data/tags.json";
+import { mockArticle } from '../data/article'
+
+test.beforeEach("go to the web-portal", async ({ page }) => {
+  await page.route("*/**/api/tags", async (overwrightRoute) => {
+    await overwrightRoute.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
+
+  await page.route("*/**/api/articles*", async (overrideFirstArticle) => {
+    const response = await overrideFirstArticle.fetch();
+    const responseBody = await response.json();
+    responseBody.articles[0].title = mockArticle.title;
+    responseBody.articles[0].description = mockArticle.description;
+    await overrideFirstArticle.fulfill({
+      body: JSON.stringify(responseBody),
+    });
+  });
+
+  await page.waitForTimeout(500);
+
+  await page.goto("https://conduit.bondaracademy.com/");
+});
+
+test("get started link", async ({ page }) => {
+  await expect.soft(page.locator(".navbar-brand")).toHaveText("conduit");
+});
+
+test("check mock article title", async ({ page }) => {
+  await expect
+    .soft(page.locator('app-article-preview h1').first())
+    .toHaveText(mockArticle.title);
+});
+
+test("check mock article description", async ({ page }) => {
+  await expect
+    .soft(page.locator('app-article-preview p').first())
+    .toHaveText(mockArticle.description);
+});
